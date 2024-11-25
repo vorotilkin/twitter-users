05ed23c1bab5fc7050d45873c29414da2f3bbd90
Eugen Vorotilkin <vorotilkin_ea@ekb.sima-land.ru>
2024-11-25 17:10:04 +0500

    new method PasswordHashByUser

--- infrastructure/repositories/user/repository.go
+++ infrastructure/repositories/user/repository.go
@@ -2,6 +2,9 @@ package user
 
 import (
 	"context"
+	"errors"
+	"github.com/go-jet/jet/v2/postgres"
+	"github.com/jackc/pgx/v5"
 	"github.com/vorotilkin/twitter-users/domain/models"
 	"github.com/vorotilkin/twitter-users/pkg/database"
 	"github.com/vorotilkin/twitter-users/schema/gen/my_database/public/model"
@@ -45,6 +48,25 @@ func (r *Repository) Create(ctx context.Context, name, passwordHash, username, e
 	}
 
 	return toDomain(user), nil
+}
+
+func (r *Repository) FetchPasswordHashByEmail(ctx context.Context, email string) (string, error) {
+	query, args := table.User.
+		SELECT(table.User.PasswordHash).
+		WHERE(table.User.Email.EQ(postgres.Text(email))).
+		Sql()
+
+	row := r.conn.QueryRow(ctx, query, args...)
+	user := model.User{}
+
+	err := row.Scan(
+		&user.PasswordHash,
+	)
+	if err != nil && !errors.Is(err, pgx.ErrNoRows) {
+		return "", err
+	}
+
+	return user.PasswordHash, nil
 }
 
 func NewRepository(conn *database.Database) *Repository {
--- usecases/users.go
+++ usecases/users.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"github.com/vorotilkin/twitter-users/domain/models"
 	"github.com/vorotilkin/twitter-users/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type UsersRepository interface {
 	Create(ctx context.Context, name, passwordHash, username, email string) (models.User, error)
+	FetchPasswordHashByEmail(ctx context.Context, email string) (string, error)
 }
 
 type UsersServer struct {
@@ -28,6 +31,19 @@ func (s *UsersServer) Create(ctx context.Context, request *proto.CreateRequest) 
 		Username:     user.Username,
 		Email:        user.Email,
 	}, nil
+}
+
+func (s *UsersServer) PasswordHashByEmail(ctx context.Context, request *proto.PasswordHashByEmailRequest) (*proto.PasswordHashByEmailResponse, error) {
+	hash, err := s.usersRepository.FetchPasswordHashByEmail(ctx, request.GetEmail())
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	if len(hash) == 0 {
+		return nil, status.Error(codes.NotFound, "hash not found")
+	}
+
+	return &proto.PasswordHashByEmailResponse{PasswordHash: hash}, nil
 }
 
 func NewUsersServer(usersRepo UsersRepository) *UsersServer {
